bfb0521db5ea27e4f0bef5cf75d71b885156401c
Jan-schweizer-99 <jan.schweizer@hs-furtwangen.de>
2021-10-23 19:54:16 +0200

    komentare und eventlistener hinzugefuegt

--- A02_Boxes/Boxes.js
+++ A02_Boxes/Boxes.js
@@ -4,36 +4,38 @@ var Boxes;
     var color;
     var x = 0;
     var y = 0;
-    for (var i = 0; i < n; i++) {
-        y += (i == 2) ? 20 : 50;
-        x = (x + 170) % 400;
-        switch (i) {
-            case 0:
-                color = "#ff0000";
-                break;
-            case 1:
-            case 4:
-                color = "#00ff00";
-                break;
-            case 3:
-                continue;
-            default:
-                color = "#0000ff";
+    window.addEventListener("load", function () {
+        for (var i = 0; i < n; i++) { //n=5 fahre die funktion 6 mal durch
+            y += (i == 2) ? 20 : 50; //wenn i größer als 2 -->evt vergessen als 20 und 50 
+            x = (x + 170) % 400;
+            switch (i) {
+                case 0: //in falle I=0
+                    color = "#ff0000"; //Rot
+                    break; //springe aus der switch case raus
+                case 1: //Debugger springt auch in die case 4 wenn der wert 1 ist
+                case 4: //
+                    color = "#00ff00"; //Grün
+                    break; //springe aus der switch case raus 
+                case 3: //Fall I=3
+                    continue; //LUL was zur hölle ist das für ein case, das ist das unnötigste was ich jemals gesehen hab
+                default:
+                    color = "#0000ff"; //Blau
+            }
+            for (var _i = 0, _a = ["big", "medium", "small"]; _i < _a.length; _i++) { //
+                var size = _a[_i];
+                createBox(color, x, y, size);
+                if (i == 4)
+                    break;
+            }
         }
-        for (var _i = 0, _a = ["big", "medium", "small"]; _i < _a.length; _i++) {
-            var size = _a[_i];
-            createBox(color, x, y, size);
-            if (i == 4)
-                break;
-        }
-    }
+    });
     function createBox(_color, _x, _y, _size) {
         var div = document.createElement("div");
         document.body.appendChild(div);
         div.classList.add(_size);
-        div.style.backgroundColor = _color;
-        div.style.left = _x + "px";
-        div.style.top = _y + "px";
+        div.style.backgroundColor = _color; //erstze farbe
+        div.style.left = _x + "px"; //verkette neues x mit px
+        div.style.top = _y + "px"; //verkette neues y mit px 
     }
 })(Boxes || (Boxes = {}));
 //# sourceMappingURL=Boxes.js.map
--- A02_Boxes/Boxes.ts
+++ A02_Boxes/Boxes.ts
@@ -3,21 +3,21 @@ namespace Boxes {
     let color: string;
     let x: number = 0;
     let y: number = 0;
-
-    for (let i: number = 0; i < n; i++) {         //n=5 fahre die funktion 6 mal durch
-        y += (i == 2) ? 20 : 50;                  //wenn i größer als 2 -->evt vergessen als 20 und 50 
+    window.addEventListener("load", function(): void {      //Habe dich garnicht vermisst schoen das du wieder da bist. Werde dich nie wieder vergessen (= Ab heute bist du mein bester Freund und wir geniesen zusammen den Sonnenaufgang.
+    for (let i: number = 0; i < n; i++) {         //n=5 fahre die funktion 5 mal durch
+        y += (i == 2) ? 20 : 50;                  //wenn i = 2 -->evt vergessen als 20 und 50 
         x = (x + 170) % 400;                      
         switch (i) {                            
             case 0:                             //in falle I=0
                 color = "#ff0000";              //Rot
                 break;                          //springe aus der switch case raus
-            case 1:                             //fall I=1
-            case 4:                             //Fall I=4 vor fall 3 -->zu fall 2 abändern das der Ablauf wieder normal ist und farben abändern
+            case 1:                             //Debugger springt auch in die case 4 wenn der wert 1 ist
+            case 4:                             //
                 color = "#00ff00";              //Grün
                 break;                          //springe aus der switch case raus 
             case 3:                             //Fall I=3
-                continue;                       //LUL was zur hölle ist das für ein case, das ist das unnötigste was ich jemals gesehen hab
-            default:                            
+                continue;                       //Debugger: springt wieder nach oben
+            default:                            //Debugger: default wird aufgerufen wenn fall I = 2  
                 color = "#0000ff";              //Blau
         }
         
@@ -27,7 +27,7 @@ namespace Boxes {
                 break;
         }
     }
-
+});
 
     function createBox(_color: string, _x: number, _y: number, _size: string): void {
         let div: HTMLDivElement = document.createElement("div");
