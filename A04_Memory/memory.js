85a5909dcd1d1a12033d64e8be4b98cf62c88939
Jan-schweizer-99 <jan.schweizer@hs-furtwangen.de>
2021-11-12 09:31:29 +0100

    HTML and typescripts first things

--- A04_Memory/memory.js
+++ A04_Memory/memory.js
@@ -1,7 +1,18 @@
 var random_test;
 (function (random_test) {
-    var list = ["klaus", "peter", "Dieta", "HÄRBERT"];
-    list = list.sort(function () { return Math.random() - 0.5; });
-    console.log(list);
+    //let list: string[] = ["klaus", "peter", "Dieta", "HÄRBERT"];
+    //list = list.sort(() => Math.random() - 0.5);
+    //console.log(list);
+    //  window.addEventListener("load", handleLoad); //starte die handleload funktion
+    //  function handleLoad(_event: Event): void {
+    //window.prompt.apply(Load);
+    var codeword = prompt("Please enter your name", "EIA-2-stinkt");
+    var chars = codeword.split("");
+    console.log(chars);
+    var counter = 0;
+    //  }
+    function Load() {
+        console.log("hat geklapt");
+    }
 })(random_test || (random_test = {}));
 //# sourceMappingURL=memory.js.map
--- A04_Memory/memory.ts
+++ A04_Memory/memory.ts
@@ -1,5 +1,17 @@
 namespace random_test {
-    let list: string[] = ["klaus", "peter", "Dieta", "HÄRBERT"];
-    list = list.sort(() => Math.random() - 0.5);
-    console.log(list);
+    //let list: string[] = ["klaus", "peter", "Dieta", "HÄRBERT"];
+    //list = list.sort(() => Math.random() - 0.5);
+    //console.log(list);
+
+    //  window.addEventListener("load", handleLoad); //starte die handleload funktion
+    //  function handleLoad(_event: Event): void {
+    window.prompt.call.addEventListener("click", Load);
+    let codeword: string = prompt("Please enter your name", "EIA-2-stinkt");
+    let chars: string[] = codeword.split("");
+    console.log(chars);
+    let counter: number = 0;
+    //  }
+    function Load(): void {
+        console.log("hat geklapt");
     }
+}
